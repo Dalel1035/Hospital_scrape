e609a00917ebebdb4fbf6e22bc5a895b869a0f32
Dalel1035 <45031760+Dalel1035@users.noreply.github.com>
2020-01-29 12:32:50 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,8 +1,33 @@
 # This is a template for a Python scraper on morph.io (https://morph.io)
 # including some code snippets below that you should find helpful
 
-# import scraperwiki
-# import lxml.html
+import scraperwiki
+import urlparse
+import lxml.xml
+
+def scrape_table(root):
+  rows = root.cssselect("table.Trolley tr")
+  for row in rows:
+    record = {}
+    table_cells = row.cssselect("td")
+    if table_cells:
+      record['Date']=table_cells[0].text
+      record['Hospital']=table_cells[1].text
+      record['Region']=table_cells[2].text
+      record['Trolley Total']=table_cells[3].text
+      record['Ward Total']=table_cells[4].text
+      
+      print record, '--------------'
+      scraperwiki.sql.save(["Hospital"], record)
+      
+def scrape_and_look_for_next_link(url):
+  html=scraperwiki.scrape(url)
+  print html
+  root = lxml.html.fromString(html)
+  scrape_table(root)
+  
+starting_url='http://inmo.ie/6022'
+scrape_and_look_for_next_link(starting_url)
 #
 # # Read in a page
 # html = scraperwiki.scrape("http://foo.com")
