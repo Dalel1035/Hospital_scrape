f1411d7b6268b8b4b5fd7708307d77cd6f33363e
Dalel1035 <45031760+Dalel1035@users.noreply.github.com>
2020-01-29 13:24:03 +0000

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -14,8 +14,11 @@ def scrape_table(root):
       record['Venue']=table_cells[0].text_content
       record['Address']=table_cells[1].text_content
       
-      print record, '--------------'
-      scraperwiki.sql.save(["Venue"], record)
+      print record, '--------------
+      try:
+         scraperwiki.sql.save(["Venue"], record)
+      except:
+        record = {"TD":"NO ENTRY"}
       
 def scrape_and_look_for_next_link(url):
   html=scraperwiki.scrape(url)
